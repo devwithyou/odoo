82c1bc811878404cb576b9a4bf2371279fec5e4c
devwithyou <msonly09@gmail.com>
2026-04-30 18:49:26 +0500

    Update Pharmacy Management System like real time systems

--- learning_addons/pharmacy_management/__manifest__.py
+++ learning_addons/pharmacy_management/__manifest__.py
@@ -7,11 +7,12 @@
     "installable": True,
     "data": [
         "security/ir.model.access.csv",
+        "views/pharmacy_dashboard_views.xml",
         "views/pharmacy_medicine_views.xml",
         "views/pharmacy_customer_views.xml",
         "views/pharmacy_batch_views.xml",
         "views/pharmacy_sale_views.xml",
         "views/pharmacy_menu.xml",
+        "reports/pharmacy_sale_report.xml",
     ],
 }
-
--- learning_addons/pharmacy_management/models/__init__.py
+++ learning_addons/pharmacy_management/models/__init__.py
@@ -2,4 +2,4 @@ from . import pharmacy_medicine
 from . import pharmacy_customer
 from . import pharmacy_batch
 from . import pharmacy_sale
-
+from . import pharmacy_dashboard
